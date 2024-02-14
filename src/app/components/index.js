02ef44212ee384dd668ff1cf129f958504e8477a
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-14 12:24:52 +0500

    resolve the component import conflict.

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,6 +1,7 @@
 export { default as Container } from "../components/Container";
 export { default as Divider } from "../components/Divider";
 export { default as HamburgerIcon } from "../components/HamburgerIcon";
+export { default as IconItem } from "../components/IconItem";
 
 //sections
 

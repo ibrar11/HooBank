8cbc2feb23b264f85f7d8b7da13541c88339bdbf
Ibrar Munawar <ibrarmunawar0@gmail.com>
2024-02-16 19:34:03 +0500

    change text opacity of text.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ text = "", onClick = undefined }) => {
   return (
     <button
       onClick={onClick}
-      className="from-light-cyan to-m-turquoise rounded-lg bg-gradient-to-br to-40% px-5 py-3 text-lg font-medium hover:bg-gradient-to-tl"
+      className="from-light-cyan to-m-turquoise rounded-lg bg-gradient-to-br to-40% px-5 py-3 text-lg font-medium text-black hover:bg-gradient-to-tl"
     >
       {text}
     </button>
